2e5d67acb13603878ca1b4d34c8064dbba0e0030
Ke Huang <kehuan@microsoft.com>
2017-11-20 09:53:59 -0500

    Added telemetric

--- Code/runme.py
+++ Code/runme.py
@@ -7,10 +7,14 @@ from multiprocessing import cpu_count
 from step1 import run_step1
 from step2 import run_step2
 from step3 import run_step3, copyFigures, visualizeTopic, saveModel
+from azureml.logging import get_azureml_logger
 
 
 
 if __name__ == '__main__':
+    aml_logger = get_azureml_logger()   # logger writes to AMLWorkbench runtime view
+    aml_logger.log('amlrealworld.document-collection-analysis.runme', 'true')
+
     logging.basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     """
@@ -29,11 +33,11 @@ if __name__ == '__main__':
     """
     Train multiple LDA models and evaluate their performance
     """
-    run_step3(topicConfig=[10, 20, 30, 40, 50, 60, 70, 80], 
-                test_ratio=0.005,
-                saveModel=False, 
-                coherence_types=['u_mass', 'c_v'])
-    copyFigures()
+    # run_step3(topicConfig=[10, 20, 30, 40, 50, 60, 70, 80], 
+    #             test_ratio=0.005,
+    #             saveModel=False, 
+    #             coherence_types=['u_mass', 'c_v'])
+    # copyFigures()
 
     """
     Train one topic model
